1ee0c516da6d739dcab5cc441b3f3323ba604034
Andrii Yeremenko <andrii.yeremenko@teamdev.com>
2024-07-24 09:05:26 +0200

    test: add negative test case for update_scheduler.go.

--- cmd/web_server/update_scheduler_test.go
+++ cmd/web_server/update_scheduler_test.go
@@ -2,6 +2,7 @@ package web_server
 
 import (
 	"bytes"
+	"errors"
 	"log"
 	"testing"
 	"time"
@@ -17,10 +18,12 @@ func (m *MockManager) UpdateAllSources() error {
 	return m.UpdateAllSourcesFunc()
 }
 
-// TestUpdateScheduler_Start tests the Start method of the UpdateScheduler.
+// TestUpdateScheduler_LogMessages tests the Start method of the UpdateScheduler for positive cases.
 func TestUpdateScheduler_LogMessages(t *testing.T) {
 	var buf bytes.Buffer
+	originalLogOutput := log.Writer()
 	log.SetOutput(&buf)
+	defer log.SetOutput(originalLogOutput)
 
 	mockManager := &MockManager{
 		UpdateAllSourcesFunc: func() error {
@@ -38,6 +41,9 @@ func TestUpdateScheduler_LogMessages(t *testing.T) {
 	}()
 
 	time.Sleep(timeout * 3)
+	scheduler.Stop()
+
+	<-done
 
 	expectedMessages := []string{
 		"Updating resources...",
@@ -48,6 +54,40 @@ func TestUpdateScheduler_LogMessages(t *testing.T) {
 		if !containsLogMessage(&buf, expectedMsg) {
 			t.Errorf("Expected log message '%s' was not found", expectedMsg)
 		}
+	}
+}
+
+// TestUpdateScheduler_ErrorLog tests the Start method of the UpdateScheduler for negative cases.
+func TestUpdateScheduler_ErrorLog(t *testing.T) {
+	var buf bytes.Buffer
+	originalLogOutput := log.Writer()
+	log.SetOutput(&buf)
+	defer log.SetOutput(originalLogOutput)
+
+	mockManager := &MockManager{
+		UpdateAllSourcesFunc: func() error {
+			return errors.New("update failed")
+		},
+	}
+
+	timeout := time.Millisecond * 100
+	scheduler := NewUpdateScheduler(mockManager, timeout)
+
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		scheduler.Start()
+	}()
+
+	time.Sleep(timeout * 3)
+	scheduler.Stop()
+
+	<-done
+
+	expectedErrorMessage := "Failed to update resources: update failed"
+
+	if !containsLogMessage(&buf, expectedErrorMessage) {
+		t.Errorf("Expected log message '%s' was not found", expectedErrorMessage)
 	}
 }
 
