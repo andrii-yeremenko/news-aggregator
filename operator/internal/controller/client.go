bc3773cd04333ddc9a44114491aaebea356d1fb8
Andrii Yeremenko <andrii.yeremenko@teamdev.com>
2024-09-23 20:00:10 +0200

    docs: add more docs to http client.

--- operator/internal/controller/client.go
+++ operator/internal/controller/client.go
@@ -6,10 +6,12 @@ import (
 	"net/http"
 )
 
+// DefaultHTTPClient is an interface for making HTTP requests.
 type DefaultHTTPClient struct {
 	client *http.Client
 }
 
+// NewDefaultHTTPClient creates a new DefaultHTTPClient.
 func NewDefaultHTTPClient() *DefaultHTTPClient {
 	return &DefaultHTTPClient{
 		client: &http.Client{
@@ -20,10 +22,12 @@ func NewDefaultHTTPClient() *DefaultHTTPClient {
 	}
 }
 
+// Do sends an HTTP request and returns an HTTP response.
 func (c *DefaultHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	return c.client.Do(req)
 }
 
+// Post sends an HTTP POST request and returns an HTTP response.
 func (c *DefaultHTTPClient) Post(url, contentType string, body io.Reader) (*http.Response, error) {
 	return c.client.Post(url, contentType, body)
 }
