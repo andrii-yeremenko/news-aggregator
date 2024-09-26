52482e875d64235d0f5d44ac44a82231c04b09cd
Andrii Yeremenko <andrii.yeremenko@teamdev.com>
2024-09-26 15:34:51 +0200

    refactor: move `ConditionType` to condition.go.

--- operator/api/v1/condition.go
+++ operator/api/v1/condition.go
@@ -15,3 +15,17 @@ type Condition struct {
 	// LastUpdateTime is the last time the condition was updated
 	LastUpdateTime metav1.Time `json:"lastUpdateTime,omitempty"`
 }
+
+// ConditionType defines the type of the condition
+type ConditionType string
+
+const (
+	// ConditionAdded means the feed has been added successfully
+	ConditionAdded ConditionType = "Added"
+	// ConditionUpdated means the feed has been updated successfully
+	ConditionUpdated ConditionType = "Updated"
+	// ConditionDeleted means the feed has been deleted successfully
+	ConditionDeleted ConditionType = "Deleted"
+	// ConditionFailed means the feed has failed to be added, updated or deleted
+	ConditionFailed ConditionType = "Failed"
+)
